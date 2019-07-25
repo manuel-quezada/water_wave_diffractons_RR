a4e5b0bbd628f6cab262f205ee1825c6dc7b144a
mquezada <manuel.quezada@kaust.edu.sa>
2019-07-25 18:32:03 +0300

    change plot_dispRelations in matlab

--- misc/dispRelations_plot/plot_dispRelations.m
+++ misc/dispRelations_plot/plot_dispRelations.m
@@ -1,8 +1,8 @@
 clc; clf 
 
 % bA, bB grid %
-bA=linspace(0,0.74,100);
-bB=linspace(0,0.74,100);
+bA=linspace(0,0.74,200);
+bB=linspace(0,0.74,200);
 [bbA,bbB]=meshgrid(bA,bB);
 
 % eta bar %
@@ -12,22 +12,17 @@ eta0Bar = eta0-0.5*(bbA+bbB);
 alpha = @(bA,bB,eta0) (bA-bB).^2/192./(eta0-bA)./(eta0-bB);
 KdVCoeff = @(bA,bB,eta0) 1/3*(eta0-0.5*(bA+bB)).^2;
 
-%alpha = (bbA-bbB).^2/192./(eta0-bbA)./(eta0-bbB);
-%KdVCoeff = 1/3*eta0Bar.^2;
-
 % Plot the coefficients %
-p1 = surf(bbA,bbB,alpha(bbA,bbB,0.75));
+p1 = surf(bbA,bbB,alpha(bbA,bbB,0.75)/2.0);
 AxOne = get(p1,'Parent');
 colormap(AxOne,'hot');
 set(p1,'edgecolor','none')
 freezeColors 
-%colormap winter
 
 hold on
-p2 = surf(bbA,bbB,KdVCoeff(bbA,bbB,0.75));
+p2 = surf(bbA,bbB,KdVCoeff(bbA,bbB,0.75)/2.0);
 AxTwo = get(p2,'Parent');
 set(p2,'edgecolor','none')
-%colormap(AxTwo,'cool');
 
 % format the figure %
 xlabel('bA','fontsize',20)
@@ -38,10 +33,15 @@ ylim([0,0.75])
 ax = gca;
 ax.FontSize = 20;
 
-%campos([-4.95    3.90    0.82])
-campos([-5.5, -1.75, 1.25])
+ax.ZTick=[0,0.04,0.08,0.12,0.16,0.2];
+ax.XTick=[0,0.2, 0.4, 0.6];
+ax.YTick=[0,0.2, 0.4, 0.6];
 
-set(gcf, 'Position',  [2000, 100, 950, 800])
+%campos([-4.95  3.90    0.82])
+campos([-5.5, -1.75, 0.7])
+
+set(gcf, 'Position',  [2000, 100, 800, 800])
+saveas(gcf,'dispCoeff.png')
 
 % Compute the ratio of the coefficients for the situations of interest %
 KdVCoeff(0,0.5,0.75)/alpha(0,0.5,0.75)
